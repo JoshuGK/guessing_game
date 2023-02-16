2082dcfe20dbd185112ffb9c0e761efb5dac397b
jog <gsakajoshua166@gmail.com>
2023-02-16 10:20:31 +0300

    added you loose

--- game.py
+++ game.py
@@ -6,11 +6,24 @@
 #using a while loop:
 #check uif a user input is equal to generated number
 from random import randint
-user_name=input("whats your name")
+user_name=input("whats your name:")
 print("hello there" + user_name + "!")
-
+random_number=randint(1,50)
 counter=0
 while counter < 5:
     usernumber=eval(input("input your number:"))
-
-number =randint(10,50)
+    counter +=1
+    if usernumber<random_number:
+        print("your guess is too low")
+    elif usernumber>random_number:
+        print("your guess is too high")
+    elif usernumber==random_number:
+    
+        break
+print("game over:")
+if usernumber==random_number:
+    print("you win!")
+    print("the correct number was",random_number)
+else:
+    print("game over! the correct numberis")
+    print(random_number)
